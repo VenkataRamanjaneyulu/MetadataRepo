f6afb630557ba379588c417a8f3c2c6b88f150a5
Venkat <kattamuruanil@gmail.com>
2021-07-19 21:28:39 +0530

    deleted

--- force-app/main/default/lwc/unitTest/__tests__/sum.test.js
+++
@@ -1,9 +0,0 @@
-import { sum } from '../sum';
-    
-describe('sum()', () => {
-  it('should add 1 and 2 returning 3', () => {
-    expect(sum(1, 2)).toBe(3);
-    expect(sum(1, 2)).not.toBeGreaterThan(3);
-    expect(sum(1, 2)).not.toBeLessThan(3);
-  });
-});
